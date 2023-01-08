bfe58a47bd8d70d79248d9fd30a9d6c89a2875cc
Ilya Shishkov <shishkovilja@gmail.com>
2023-01-08 17:49:25 +0300

    WIP: java doc

--- src/main/java/dev/ignitop/ui/component/TerminalComponent.java
+++ src/main/java/dev/ignitop/ui/component/TerminalComponent.java
@@ -8,8 +8,10 @@ import dev.ignitop.ui.Terminal;
 @SuppressWarnings("UnnecessaryModifier")
 public interface TerminalComponent {
     /**
+     * Render component in a given terminal. Component will be expanded or shrinked to a given size.
+     *
      * @param terminal Terminal.
-     * @param width Width.
+     * @param width Desired width.
      */
     public void render(Terminal terminal, int width);
 
