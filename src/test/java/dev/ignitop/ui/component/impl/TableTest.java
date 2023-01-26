0f113f0ff56f28be2cb0a92712275c42156b8d6d
Ilya Shishkov <shishkovilja@gmail.com>
2023-01-27 00:42:13 +0300

    WIP: TableTest

--- src/test/java/dev/ignitop/ui/component/impl/TableTest.java
+++ src/test/java/dev/ignitop/ui/component/impl/TableTest.java
@@ -1,5 +1,6 @@
 package dev.ignitop.ui.component.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -20,29 +21,14 @@ class TableTest {
      */
     @Test
     void render_withExpanding() {
-        String veryWideHdr = "Very very very wide header";
-        String narrowHdr = "Narrhdr";
+        // Calculating by widest element:
+        // "Very very very wide header  Content [24,1]  "
+        // Col1: 26 + 2 (gap) = 28      Col2: 14 + 2 (gap) = 16
+        // Delta = 80 - (28 + 16) = 36
+        // Col1: 28 * 36 / 44 -> expand on 22 -> 50 (36 - 22 = 14 of delta left) -> minus 2 gap -> 48
+        // Col2: 16 * 36 / 44 -> expand on 13 -> 29 (1 of delta left) -> 30 -> minus 2 gap -> 28
 
-        List<String> hdr = List.of(veryWideHdr, narrowHdr);
-
-        List<List<?>> rows = rows(5);
-
-        String renderedTable = TestUtils.renderToString(new Table(hdr, rows), 80);
-
-        // Header + rows + total items line
-        assertEquals(rows.size() + 2, renderedTable.lines().count());
-
-        int cellsGap = 2;
-
-        // Width is derived from the widest element, i.e. column header name itself
-        int expWideColWidth = veryWideHdr.length() + cellsGap + 24;
-
-        // Width is derived from the widest element, i.e. from column content
-        int expNarrowColWidth = "Content [x,y]".length() + cellsGap + 13;
-
-        String expTable = table(List.of(veryWideHdr, narrowHdr), rows, List.of(expWideColWidth, expNarrowColWidth));
-
-        assertEquals(expTable, renderedTable);
+        checkTable(80, List.of("Very very very wide header", "Narrhdr"), 25, List.of(48, 28));
     }
 
     /**
@@ -50,7 +36,13 @@ class TableTest {
      */
     @Test
     void render_withContentShrinking() {
-        fail("Unimplemented");
+        // Calculating by widest element:
+        // "Content [24,1]  Content [24,1]  "
+        // Col1: 14 + 2 (gap) = 16      Col2: 14 + 2 (gap) = 16
+        // Delta = 20 - (16 + 16) = -12
+        // Col1: -12 * 16 / 32 -> shrink on 6 -> 10 (-12 - (-6) = -6 of delta left) -> minus 2 gap -> 8
+        // Col2: -12 * 16 / 32 -> shrink on 6 -> 10 (0 of delta left) -> minus 2 gap -> 8
+        checkTable(20, List.of("Narrhdr", "Narrhdr"), 25, List.of(8, 8));
     }
 
     /**
@@ -157,24 +149,69 @@ class TableTest {
     }
 
     /**
+     * @param renderWidth Render width.
      * @param hdr Header.
-     * @param rows Rows.
+     * @param rowsCnt Rows count.
+     * @param expColWidths Expected column widths, excluding column gap.
+     */
+    private void checkTable(int renderWidth, List<String> hdr, int rowsCnt, List<Integer> expColWidths) {
+        List<List<?>> rows = rows(rowsCnt);
+
+        String renderedTable = TestUtils.renderToString(new Table(hdr, rows), renderWidth);
+
+        // Header + rows + total items line.
+        assertEquals(rows.size() + 2, renderedTable.lines().count());
+
+        ArrayList<List<String>> rowsWithHdr = new ArrayList<>();
+
+        rowsWithHdr.add(hdr);
+
+        for (List<?> row : rows) {
+            rowsWithHdr.add(row.stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList()));
+        }
+
+        // Shrink if neccessary
+        for (int i = 0; i < rowsWithHdr.size(); i++) {
+            for (int j = 0; j < rowsWithHdr.get(i).size(); j++) {
+                String cell = rowsWithHdr.get(i).get(j);
+
+                if (cell.length() > expColWidths.get(j))
+                    rowsWithHdr.get(i).set(j, cell.substring(0, expColWidths.get(j)));
+            }
+        }
+
+        String expTable = table(rowsWithHdr, expColWidths);
+
+        assertEquals(expTable, renderedTable);
+    }
+
+    /**
+     * @param rowsWithHdr Rows with a header.
      * @param colWidths Column widths.
      */
-    private String table(List<String> hdr, List<List<?>> rows, List<Integer> colWidths) {
-        String rowFormat = String.format("%%-%ds".repeat(colWidths.size()), colWidths.toArray());
+    private String table(List<List<String>> rowsWithHdr, List<Integer> colWidths) {
+        int colGap = 2;
+
+        String rowFormat = String.format(
+            "%%-%ds".repeat(colWidths.size()),
+            colWidths.
+                stream()
+                .map(i -> i + colGap)
+                .toArray());
 
         String expHdr = ansi()
             .fgBlack()
             .bgGreen()
-            .a(String.format(rowFormat, hdr.toArray()))
+            .a(String.format(rowFormat, rowsWithHdr.get(0).toArray()))
             .reset().toString();
 
-        String expRows = rows.stream()
+        String expRows = rowsWithHdr.subList(1, rowsWithHdr.size()).stream()
             .map(l -> String.format(rowFormat, l.toArray()))
             .collect(Collectors.joining(lineSeparator()));
 
-        return String.join(lineSeparator(), expHdr, expRows,"Total items: " + rows.size()) +
+        return String.join(lineSeparator(), expHdr, expRows,"Total items: " + (rowsWithHdr.size() - 1)) +
             lineSeparator();
     }
 }
